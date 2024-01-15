27c571d58dc8861b508b1f24d1064e5caae04d80
yev-kurtasova <eugenia.kurtasova@gmail.com>
2024-01-15 12:49:19 +0200

    create counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>Лічильник</h3>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count - 1)} className='minus'> - Мінус</button>
+        <button onClick={increase} className='plus'>Плюс +</button>
+
+      </div>
     </div>
   );
 }
